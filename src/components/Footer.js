0cdb6e4fcbdf63579ec56f2bd0b7705f90413a7b
kathecr <kcastelblancor@gmail.com>
2021-09-11 02:33:57 -0500

    Diseño de la pagina 404

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ import "../assets/styles/components/Footer.css";
 function Footer(){
   return(
     <footer className="footer">
-    <span>©Katherine Castelblanco, 2021</span>    
-    <span>Contacto 👋 3022183843</span>
-    <a href="www.linkedin.com/in/katherin-castelblanco" target="_blank"> Linkedin </a>
+      <span>©Katherine Castelblanco, 2021</span>    
+      <span>Contacto 👋 3022183843</span>
+      <a href="www.linkedin.com/in/katherin-castelblanco" target="_blank"> Linkedin </a>
     </footer>
   )
 }
--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "../assets/styles/NotFound.css"
 
 function NotFound(){
   return (
-    <React.Fragment>
-      <h1>
-        No encontrado
-        <Link to="/">
-          Volver a Home
-        </Link>
-      </h1>
-    </React.Fragment>
+    <div className="error">
+      <span className="error__numero">
+        404        
+      </span>
+      <span className="error__texto"> Page Not Found</span>
+      <Link to="/">
+        Volver a Home
+      </Link>     
+      </div>
   )
 }
 
