dccc3c2e079aa14402ce78c2d837126d64c83796
kathecr <kcastelblancor@gmail.com>
2021-09-12 20:44:30 -0500

    Integracion de React Hooks y consumo de la API agify

--- src/containers/AgePredictor.js
+++ src/containers/AgePredictor.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function AgePredictor(){
+  const [name, setName] = useState('michael');
+  const [age, setAge] = useState(0);
+  useEffect(() => {
+    fetch(`https://api.agify.io/?name=${name}`)
+      .then(res => res.json())
+      .then(data =>   setAge(data))
+  },[])
+
+  console.log(age)
   return(
     <h1>Pagina 2</h1>
-  )
+  );
 }
 
 export default AgePredictor;
